0a86bd337014c3e2f515982c9ac676a73a97e856
Hector Gonzalez <hectorgomendez@gmail.com>
2025-02-20 11:42:27 +0000

    UPDATE: Class ConversionTest and RestaTest

--- CALCULADORA/src/test/java/clases/ConversionTest.java
+++ CALCULADORA/src/test/java/clases/ConversionTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ConversionTest {
+public class ConversionTest {
 
     private Conversion conversion;
 
